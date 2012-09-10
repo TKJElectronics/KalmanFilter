17f192ecaeb4c5fbf238a814bfdff68756212cec
Kristian Lauszus <lauszus@gmail.com>
2012-09-10 02:25:09 +0200

    Minor edit

--- Kalman.h
+++ Kalman.h
@@ -25,6 +25,7 @@ public:
         Q_angle = 0.001;
         Q_bias = 0.003;
         R_measure = 0.03;
+        
         bias = 0; // Reset bias
         P[0][0] = 0; // Since we assume tha the bias is 0 and we know the starting angle (use setAngle), the error covariance matrix is set like so - see: http://en.wikipedia.org/wiki/Kalman_filter#Example_application.2C_technical
         P[0][1] = 0;
@@ -35,14 +36,16 @@ public:
     double getAngle(double newAngle, double newRate, double dt) {
         // KasBot V2  -  Kalman filter module - http://www.x-firm.com/?page_id=145
         // Modified by Kristian Lauszus
-        // See my blog post for more information: http://blog.tkjelectronics.dk/2012/08/a-practical-approach-to-kalman-filter-and-how-to-implement-it
+        // See my blog post for more information: http://blog.tkjelectronics.dk/2012/09/a-practical-approach-to-kalman-filter-and-how-to-implement-it
                         
         // Discrete Kalman filter time update equations - Time Update ("Predict")
         // Update xhat - Project the state ahead
+        /* Step 1 */
         rate = newRate - bias;
         angle += dt * rate;
         
         // Update estimation error covariance - Project the error covariance ahead
+        /* Step 2 */
         P[0][0] += dt * (dt*P[1][1] - P[0][1] - P[1][0] + Q_angle);
         P[0][1] -= dt * P[1][1];
         P[1][0] -= dt * P[1][1];
@@ -50,16 +53,21 @@ public:
         
         // Discrete Kalman filter measurement update equations - Measurement Update ("Correct")
         // Calculate Kalman gain - Compute the Kalman gain
+        /* Step 4 */
         S = P[0][0] + R_measure;
+        /* Step 5 */
         K[0] = P[0][0] / S;
         K[1] = P[1][0] / S;
         
         // Calculate angle and bias - Update estimate with measurement zk (newAngle)
+        /* Step 3 */
         y = newAngle - angle;
+        /* Step 6 */
         angle += K[0] * y;
         bias += K[1] * y;
         
         // Calculate estimation error covariance - Update the error covariance
+        /* Step 7 */
         P[0][0] -= K[0] * P[0][0];
         P[0][1] -= K[0] * P[0][1];
         P[1][0] -= K[1] * P[0][0];
@@ -77,9 +85,9 @@ public:
     
 private:
     /* Kalman filter variables */
-    double Q_angle; // Process noise covariance for the accelerometer - (w = process noise)
-    double Q_bias; // Process noise covariance for the gyro bias - (w = process noise)
-    double R_measure; // Measurement noise covariance - this is actually the variance of the measurement noise - (v = measurement noise)
+    double Q_angle; // Process noise variance for the accelerometer
+    double Q_bias; // Process noise variance for the gyro bias
+    double R_measure; // Measurement noise variance - this is actually the variance of the measurement noise
     
     double angle; // The angle calculated by the Kalman filter - part of the 2x1 state matrix
     double bias; // The gyro bias calculated by the Kalman filter - part of the 2x1 state matrix
