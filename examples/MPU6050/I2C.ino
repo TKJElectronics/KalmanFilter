ad4efd09ba7922cd0776e4196a2cb0f3839a53e8
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-04-29 20:25:51 +0200

    Write multiple bytes at once

--- examples/MPU6050/I2C.ino
+++ examples/MPU6050/I2C.ino
@@ -2,16 +2,22 @@ const uint8_t IMUAddress = 0x68; // AD0 is logic low on the PCB
 const uint16_t I2C_TIMEOUT = 1000; // Used to check for errors in I2C communication
 
 uint8_t i2cWrite(uint8_t registerAddress, uint8_t data, bool sendStop) {
+  return i2cWrite(registerAddress,&data,1,sendStop); // Returns 0 on success
+}
+
+uint8_t i2cWrite(uint8_t registerAddress, uint8_t* data, uint8_t length, bool sendStop) {
   Wire.beginTransmission(IMUAddress);
   Wire.write(registerAddress);
-  Wire.write(data);
+  Wire.write(data, length);
   return Wire.endTransmission(sendStop); // Returns 0 on success
 }
+
 uint8_t i2cRead(uint8_t registerAddress, uint8_t* data, uint8_t nbytes) {
   uint32_t timeOutTimer;
   Wire.beginTransmission(IMUAddress);
   Wire.write(registerAddress);
-  Wire.endTransmission(false); // Don't release the bus
+  if(Wire.endTransmission(false)) // Don't release the bus
+    return 1; // Error in communication
   Wire.requestFrom(IMUAddress, nbytes,(uint8_t)true); // Send a repeated start and then release the bus after reading
   for(uint8_t i = 0; i < nbytes; i++) {
     if(Wire.available())
@@ -22,7 +28,7 @@ uint8_t i2cRead(uint8_t registerAddress, uint8_t* data, uint8_t nbytes) {
       if(Wire.available())
         data[i] = Wire.read();
       else
-        return 1; // Error in communication
+        return 2; // Error in communication
     }
   }
   return 0; // Success
--- examples/MPU6050/MPU6050.ino
+++ examples/MPU6050/MPU6050.ino
@@ -38,10 +38,11 @@ uint8_t i2cData[14]; // Buffer for I2C data
 void setup() {  
   Serial.begin(115200);
   Wire.begin();
-  while(i2cWrite(0x1B,0x00,false)); // Set Gyro Full Scale Range to ±250deg/s
-  while(i2cWrite(0x1C,0x00,false)); // Set Accelerometer Full Scale Range to ±2g
-  while(i2cWrite(0x19,7,false)); // Set the sample rate to 1000Hz
-  while(i2cWrite(0x1A,0x00,false)); // Disable FSYNC and set 260 Hz Acc filtering, 256 Hz Gyro filtering, 8 KHz sampling
+  i2cData[0] = 7; // Set the sample rate to 1000Hz - 8kHz/(7+1) = 1000Hz
+  i2cData[1] = 0x00; // Disable FSYNC and set 260 Hz Acc filtering, 256 Hz Gyro filtering, 8 KHz sampling
+  i2cData[2] = 0x00; // Set Gyro Full Scale Range to ±250deg/s
+  i2cData[3] = 0x00; // Set Accelerometer Full Scale Range to ±2g
+  while(i2cWrite(0x19,i2cData,4,false)); // Write to all four registers at once
   while(i2cWrite(0x6B,0x01,true)); // PLL with X axis gyroscope reference and disable sleep mode 
   
   while(i2cRead(0x75,i2cData,1));
