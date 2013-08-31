a1ea7ca69f679081168b7bee3eeeedb64c5695a8
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-08-31 16:08:12 +0200

    Print an error message if I2C communication fails

--- examples/MPU6050/I2C.ino
+++ examples/MPU6050/I2C.ino
@@ -5,30 +5,41 @@ uint8_t i2cWrite(uint8_t registerAddress, uint8_t data, bool sendStop) {
   return i2cWrite(registerAddress,&data,1,sendStop); // Returns 0 on success
 }
 
-uint8_t i2cWrite(uint8_t registerAddress, uint8_t* data, uint8_t length, bool sendStop) {
+uint8_t i2cWrite(uint8_t registerAddress, uint8_t *data, uint8_t length, bool sendStop) {
   Wire.beginTransmission(IMUAddress);
   Wire.write(registerAddress);
   Wire.write(data, length);
-  return Wire.endTransmission(sendStop); // Returns 0 on success
+  uint8_t rcode = Wire.endTransmission(sendStop); // Returns 0 on success
+  if (rcode) {
+    Serial.print(F("i2cWrite failed: "));
+    Serial.println(rcode);
+  }
+  return rcode; // See: http://arduino.cc/en/Reference/WireEndTransmission
 }
 
-uint8_t i2cRead(uint8_t registerAddress, uint8_t* data, uint8_t nbytes) {
+uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes) {
   uint32_t timeOutTimer;
   Wire.beginTransmission(IMUAddress);
   Wire.write(registerAddress);
-  if(Wire.endTransmission(false)) // Don't release the bus
-    return 1; // Error in communication
+  uint8_t rcode = Wire.endTransmission(false); // Don't release the bus
+  if (rcode) {
+    Serial.print(F("i2cRead failed: "));
+    Serial.println(rcode);
+    return rcode; // See: http://arduino.cc/en/Reference/WireEndTransmission
+  }
   Wire.requestFrom(IMUAddress, nbytes,(uint8_t)true); // Send a repeated start and then release the bus after reading
-  for(uint8_t i = 0; i < nbytes; i++) {
-    if(Wire.available())
+  for (uint8_t i = 0; i < nbytes; i++) {
+    if (Wire.available())
       data[i] = Wire.read();
     else {
       timeOutTimer = micros();
-      while(((micros() - timeOutTimer) < I2C_TIMEOUT) && !Wire.available());
-      if(Wire.available())
+      while (((micros() - timeOutTimer) < I2C_TIMEOUT) && !Wire.available());
+      if (Wire.available())
         data[i] = Wire.read();
-      else
-        return 2; // Error in communication
+      else {
+        Serial.println(F("i2cRead timeout"));
+        return 5; // This error value is not already taken by endTransmission
+      }
     }
   }
   return 0; // Success
