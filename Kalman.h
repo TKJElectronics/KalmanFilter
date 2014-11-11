5b6f40bb143240da7f134f41b79606c383635007
Kristian Sloth Lauszus <lauszus@gmail.com>
2014-11-10 16:39:30 -0800

    Fix bug where new the value of P00 and P01 where used instead of the old

--- Kalman.h
+++ Kalman.h
@@ -70,10 +70,13 @@ public:
 
         // Calculate estimation error covariance - Update the error covariance
         /* Step 7 */
-        P[0][0] -= K[0] * P[0][0];
-        P[0][1] -= K[0] * P[0][1];
-        P[1][0] -= K[1] * P[0][0];
-        P[1][1] -= K[1] * P[0][1];
+        float P00_temp = P[0][0];
+        float P01_temp = P[0][1];
+
+        P[0][0] -= K[0] * P00_temp;
+        P[0][1] -= K[0] * P01_temp;
+        P[1][0] -= K[1] * P00_temp;
+        P[1][1] -= K[1] * P01_temp;
 
         return angle;
     };
